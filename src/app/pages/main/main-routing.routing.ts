e6d32c1ce46bd2a413105d7c892bcdb97ea9b39f
fanchenggang <276664560@qq.com>
2018-11-05 09:58:50 +0800

    detail 页面

--- src/app/pages/main/main-routing.routing.ts
+++ src/app/pages/main/main-routing.routing.ts
@@ -1,12 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main.component';
 import { InitGuardService } from 'src/app/services/init-guard.service';
+import { DetailComponent } from './detail/detail.component';
 
 const routes: Routes = [
   {
     path: 'main',
     component: MainComponent,
-    canActivate: [ InitGuardService ]
+    canActivate: [ InitGuardService ],
+    children:[
+    ]
   }
 ];
 
--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { HeaderComponent } from './header/header.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgZorroAntdModule
   ],
-  declarations: [MainComponent, HeaderComponent]
+  declarations: [MainComponent, HeaderComponent, DetailComponent]
 })
 export class MainModule { }
